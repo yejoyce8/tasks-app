da03e411dca272ccf23b205bb5cc64330b7ea6db
Joyce Ye <yejoyce8@gmail.com>
2023-08-16 21:35:05 -0400

    createTask basic endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
+const logMiddleware = (req, res, next) => {
+    console.log('Request received: ', req.url);
+    next();
+}
+
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
+});
+
+
+app.post("/api/createTask", logMiddleware, (req, res) => {
+    const {taskName, time} = req.body;
+    const response = {taskName: "new", time: 1}
+    res.status(201).json(response);
 });
 
 app.listen(port, () => {
