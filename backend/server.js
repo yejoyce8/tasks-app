72eb890cf27e9c744cb4237601ae88ac4df6d5c6
Joyce Ye <yejoyce8@gmail.com>
2023-08-16 22:29:57 -0400

    CORS fix + createTask res = req

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 const logMiddleware = (req, res, next) => {
     console.log('Request received: ', req.url);
@@ -17,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.post("/api/createTask", logMiddleware, (req, res) => {
     const {taskName, time} = req.body;
-    const response = {taskName: "new", time: 1}
+    const response = {taskName: req.body.taskName, time: req.body.time}
     res.status(201).json(response);
 });
 
